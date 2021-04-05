695754d3a89902850a68b0bab0095722fc5a7f2d
Lef <terisyou@hotmail.com>
2021-04-05 23:14:25 +0300

    Adding a Username

--- resources/views/admin/users/profile.blade.php
+++ resources/views/admin/users/profile.blade.php
@@ -3,5 +3,71 @@
 @section('content')
 <h1>User Profile for : {{$user->name}}</h1>
 
+<div class="row">
+
+    <div class="col-sm-6">
+
+        <form method="post" action="" enctype="multipart/form-data">
+            @csrf
+
+            <div class="mb-4">
+                    <img height="150px" width="175px" class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
+            </div>
+
+            <div class="form-group">
+                    <input type="file">
+            </div>
+
+            <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text"
+                        name="username"
+                        class="form-control"
+                        id="username"
+                        value="{{$user->username}}">
+
+            <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text"
+                            name="name"
+                            class="form-control"
+                            id="name"
+                            value="{{$user->name}}">
+
+                <div class="form-group">
+                        <label for="email">email</label>
+                        <input type="text"
+                        name="email"
+                        class="form-control"
+                        id="email"
+                        value="{{$user->email}}">
+                </div>
+                <div class="form-group">
+                        <label for="password">password</label>
+                        <input type="password"
+                        name="password"
+                        class="form-control"
+                        id="password">
+
+                </div>
+                <div class="form-group">
+                        <label for="password-confirm">Confirm Password</label>
+                        <input type="password"
+                        name="password_confirmation"
+                        class="form-control"
+                        id="password-confirmation">
+
+                </div>
+
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+
+    </div>
+
+
+</div>
+
 @endsection
 </x-admin-master>
