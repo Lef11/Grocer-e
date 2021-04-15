9a022105760dadb95dce81642f23ef094aac0ead
Lef <terisyou@hotmail.com>
2021-04-15 21:44:17 +0300

    Create & Read

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -3,8 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Role;
+use Illuminate\Support\Str;
 
 class RoleController extends Controller
 {
     //
+public function index(){
+
+    return view('admin.roles.index',[
+        'roles'=> Role::all(),
+
+
+
+    ]);
+}
+public function store(){
+    request()->validate([
+        'name'=> ['required']
+
+    ]);
+    Role::create([
+        'name'=> Str::ucfirst(request('name')),
+        'slug'=> Str::of(Str::lower(request('name')))->slug('-')
+
+    ]);
+        return back();
+}
+
+
+
 }
---
+++ routes/web/roles.php
@@ -0,0 +1,5 @@
+<?php
+
+Route::get('/roles', [App\Http\Controllers\RoleController::class, 'index'])->name('roles.index');
+
+Route::post('/roles', [App\Http\Controllers\RoleController::class, 'store'])->name('roles.store');
